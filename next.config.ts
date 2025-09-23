f3d5d97b6f2a9cb017e77719090c6bec5982f9ba
Ahmed-Shawki-Dev <ahmedshawki.dev@gmail.com>
2025-09-24 01:11:34 +0300

    remove images protocol

--- next.config.ts
+++ next.config.ts
@@ -7,14 +7,6 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
       },
-      {
-        protocol: 'https',
-        hostname: 'loremflickr.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-      },
     ],
   },
 }
